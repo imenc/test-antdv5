322363f2aefe61d4443eb22a509fd825dd7335ac
imenc <imenc@live.com>
2025-02-11 08:22:19 +0700

    running antd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import withTM from "next-transpile-modules";
+
+const nextConfig = withTM([
+  "antd",
+  "rc-util",
+  "rc-pagination",
+  "rc-select",
+  "rc-tooltip",
+  "rc-picker",
+  "@ant-design/icons",
+  "@ant-design/icons-svg",
+])({
   reactStrictMode: true,
-};
+  experimental: { appDir: false }, // Disable App Router if needed
+});
 
 export default nextConfig;
+
